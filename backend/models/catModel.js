f05a0e9f66959f367e5e4f3a7a483f5df28873a5
Majdkatbeh7 <majdkatbeh2000@gmail.com>
2024-07-25 08:06:25 -0700

    edit

--- backend/models/catModel.js
+++ backend/models/catModel.js
@@ -5,7 +5,7 @@ const catSchema = new mongoose.Schema({
     //   cat: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
     catName: { type: String, required: true },
     catAge: { type: Number, required: true },
-    catWeight: { type: Number, default: 0.5 },
+    catWeight: { type: Number, required:true },
     catImage: { type: String, require: true }
 });
 

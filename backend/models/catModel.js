fc7093352896dd91ae7fd773d54ac0f4644d169c
khaledabdalslam99 <khaledabdalslam99@gmail.com>
2024-07-25 11:05:27 -0400

    cat controller done

--- backend/models/catModel.js
+++ backend/models/catModel.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const catSchema = new mongoose.Schema({
 
     //   cat: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-    catName: { type: String, required: true },
+    catName: { type: String, required: true , unique: true },
     catAge: { type: Number, required: true },
     catWeight: { type: Number, default: 0.5 },
     catImage: { type: String, require: true }

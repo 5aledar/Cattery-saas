2caf0c11bb2cf1daa03c810468b89a5cb8174e1c
BayanAlbaghdadi0 <ham123cam12@gmail.com>
2024-07-25 21:48:29 +0200

    - edit page added

--- frontend/src/componants/Nav.jsx
+++ frontend/src/componants/Nav.jsx
@@ -7,19 +7,30 @@ import Button from "@mui/material/Button";
 // import IconButton from '@mui/material/IconButton';
 // import MenuIcon from '@mui/icons-material/Menu';
 import { PiCatFill } from "react-icons/pi";
+import Tooltip from "@mui/material/Tooltip";
 import { FaPlus } from "react-icons/fa6";
+import { Link } from "react-router-dom";
 export function Nav() {
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
         <Toolbar>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            News
+            Admin
           </Typography>
-          <Button color="inherit"  variant="outlined" size="large">
-            <PiCatFill className="text-xl" />
-            <FaPlus className="text-xl" />
-          </Button>
+          <Tooltip title="Add" arrow>
+            <Link to="/add">
+              <Button
+                className="flex gap-2"
+                color="inherit"
+                variant="outlined"
+                size="large"
+              >
+                <PiCatFill className="text-xl" />
+                <FaPlus className="text-xl" />
+              </Button>
+            </Link>
+          </Tooltip>
         </Toolbar>
       </AppBar>
     </Box>
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+import daisyui from "daisyui"
 export default {
   content: [
     "./index.html",
@@ -8,6 +9,8 @@ export default {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    daisyui,
+  ],
 }
 

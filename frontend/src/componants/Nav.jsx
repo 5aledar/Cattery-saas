afa7e12df88c268e05da82f36b0899452c23f38a
BayanAlbaghdadi0 <ham123cam12@gmail.com>
2024-07-25 17:43:34 +0200

    -vew

--- frontend/src/componants/Nav.jsx
+++ frontend/src/componants/Nav.jsx
@@ -1,30 +1,25 @@
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
-
-export  function Nav() {
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+// import IconButton from '@mui/material/IconButton';
+// import MenuIcon from '@mui/icons-material/Menu';
+import { PiCatFill } from "react-icons/pi";
+import { FaPlus } from "react-icons/fa6";
+export function Nav() {
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
         <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             News
           </Typography>
-          <Button color="inherit">Login</Button>
+          <Button color="inherit" variant="outlined" size="large">
+            <PiCatFill className="text-xl" />
+            <FaPlus className="text-xl" />
+          </Button>
         </Toolbar>
       </AppBar>
     </Box>

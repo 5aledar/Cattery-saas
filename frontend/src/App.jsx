b7b656b130687ffb25c6b82879d3de4808dbaf0b
Ahmadmaherhb <abomaherhb617@gmail.com>
2024-07-26 12:36:06 +0300

    -fghjkl

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,16 @@
 import Button from '@mui/material/Button';
 import { Navigate, Route, Routes } from "react-router-dom";
 import {Home} from './pages/Home'
-import {Dashboard} from './pages/dashboard/Dashboard'
+// import {Dashboard} from './pages/dashboard/Dashboard'
 
 function App() {
   return (
     <>
     <Routes>
       <Route path='/' element={<Home></Home>}></Route>
-      <Route path='/dashboard' element={<Dashboard></Dashboard>}></Route>
+      {/* <Route path='/dashboard' element={<Dashboard></Dashboard>}></Route> */}
     </Routes>
+    
     </>
   );
 }
--- frontend/src/componants/Nav.jsx
+++ frontend/src/componants/Nav.jsx
@@ -4,22 +4,23 @@ import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
-// import IconButton from '@mui/material/IconButton';
-// import MenuIcon from '@mui/icons-material/Menu';
-import { PiCatFill } from "react-icons/pi";
-import { FaPlus } from "react-icons/fa6";
+import { FaCat, FaPlus } from "react-icons/fa";
+import Tooltip from '@mui/material/Tooltip';
+
 export function Nav() {
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
         <Toolbar>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            News
+            Admin
           </Typography>
-          <Button color="inherit"  variant="outlined" size="large">
-            <PiCatFill className="text-xl" />
-            <FaPlus className="text-xl" />
-          </Button>
+          <Tooltip title="Add" arrow>
+            <Button color="inherit" variant="outlined" size="large">
+              <FaCat className="text-xl" />
+              <FaPlus className="text-xl" />
+            </Button>
+          </Tooltip>
         </Toolbar>
       </AppBar>
     </Box>

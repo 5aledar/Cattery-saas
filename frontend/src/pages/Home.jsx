ead7d4755190df3a14d242e3becb259124c58b85
Majdkatbeh7 <majdkatbeh2000@gmail.com>
2024-07-26 07:31:45 -0700

    hi

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Nav } from '../componants/Nav'
+import AboutUs from '../componants/AboutUs'
 // import { CatAnimation } from './CatAnimation'
 export const Home = () => {
   return (
     <>
     <Nav></Nav>
-    <div>Home</div>
+    <AboutUs/>
+
     {/* <CatAnimation></CatAnimation> */}
     </>
   )
